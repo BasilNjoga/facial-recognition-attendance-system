92edd10cc02706aecd141f8e22579136a986e4dc
Basil Njoga <Basilnjoga@gmail.com>
2023-04-15 22:20:51 +0300

    added mysql, sqlalchemy and pysql integration for databases

--- config.py
+++ config.py
@@ -1,7 +1,30 @@
 from flask import Flask
+from flask_wtf import FlaskForm
+from wtfforms import StringField, SubmitField
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+# Create a Flask Instance
 app = Flask(__name__)
+# Add Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/facialrecognition'
+# Secret Key !
+app.config['SECRET_KEY'] = "my super secret key"
+# Initialize The Database
+app.app_context().push()
+db = SQLAlchemy(app)
+
+#Create Model
+class Myusers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    #create A string
+    def __repr__(self):
+        return '<Name %r>' % self.name
 
 @app.route("/")
 def home():
