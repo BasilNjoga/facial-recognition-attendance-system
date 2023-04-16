74faac675c5064cc95cb18ffff89521ba3a5932d
Basil Njoga <Basilnjoga@gmail.com>
2023-04-17 00:51:54 +0300

    created database connection to forms with sqlite for initial testing

--- config.py
+++ config.py
@@ -61,7 +61,20 @@ def name():
 def add_user():
     name = None
     form = UserForm()
-    return render_template("add_user.html", form = form)
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template("add_user.html",
+        form = form,
+        name = name,
+        our_users=our_users)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
