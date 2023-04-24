a49b75d4f2e1e7da6a30a344c3e64f1fae4a46e1
Basil Njoga <Basilnjoga@gmail.com>
2023-04-24 16:03:09 +0300

    added password hashing function and added password field to model

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, flash
 from flask import request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired , EqualTo, Length
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date, datetime
 import numpy as np
@@ -11,6 +11,7 @@ import pandas as pd
 import joblib
 import cv2
 import os
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # Create a Flask Instance
 app = Flask(__name__)
@@ -27,6 +28,19 @@ class Users(db.Model):
     name = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    # Do somme password stuff
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute!')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     #create A string
     def __repr__(self): 
@@ -41,7 +55,8 @@ class UserForm(FlaskForm):
 class NamerForm(FlaskForm):
     name = StringField("Name:", validators=[DataRequired()])
     email = StringField("Email:", validators=[DataRequired()])
-    
+    password_hash = PasswordField('Password', validators=[DataRequired(), EqualTo('password_hash2', message='Passwords Must Match')])
+    password_hash2 = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField("Log in")
 
 #Adding machine model functions 
@@ -168,13 +183,20 @@ def name():
     form = NamerForm()
     #Validate Form
     if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.sesion.add(user)
+            db.session.commit()
         name = form.name.data
         form.name.data = ''
+        form.email.data = ''
         flash("Form Submitted Successfuly")
-
+    our_users = Users.query.order_by(Users.date_added)
     return render_template('name.html',
         name = name,
-        form = form)
+        form = form,
+        our_users = our_users)
 
 @app.route("/add_user", methods=['GET', 'POST'])
 def add_user():
